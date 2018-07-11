fe47311c75ce2ce1915f4f0139873261650780ab
typesAreSpaces <jabelcastellanosjoo@unm.edu>
2018-07-11 10:20:03 +0800

    changes

--- Software/Cpp/FastCongruenceClosure/code5/include/congruenceClosure.h
+++ Software/Cpp/FastCongruenceClosure/code5/include/congruenceClosure.h
@@ -1,16 +1,14 @@
 #ifndef CONG_CLOSURE
 #define CONG_CLOSURE
 
-#include "unionfind.h"
-#include "signatureTable.h"
-#include <string>
-#include <set>
+#include <iostream>
 
-typedef std::vector<linkedList> term;
-typedef std::set<int> setSingleton;
-typedef std::set<std::pair<int, int> > setPair;
-
-UF initializeUF(int &, int &, term &, std::string);
-void congruenceClosureAlgorithm(term & , int , UF &, signatureTable &);
+class CongruenceClosure{
+ private:
+ public:
+  CongruenceClosure();
+  ~CongruenceClosure();
+  void algorithm();
+};
 
 #endif
