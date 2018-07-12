8a3640814fa7a6367a5eea77fae3c07a28481107
typesAreSpaces <jabelcastellanosjoo@unm.edu>
2018-07-12 14:31:43 +0800

    debugging points added

--- Software/Cpp/FastCongruenceClosure/code5/include/congruenceClosure.h
+++ Software/Cpp/FastCongruenceClosure/code5/include/congruenceClosure.h
@@ -9,6 +9,11 @@
 
 typedef std::set<Vertex*> Pending;
 typedef std::set<std::pair<Vertex*, Vertex*> > Combine;
+extern bool traceMerge;
+extern bool traceCombine;
+extern bool tracePending;
+extern bool traceEC;
+extern bool traceSigTable;
 
 class CongruenceClosure{
  private:
