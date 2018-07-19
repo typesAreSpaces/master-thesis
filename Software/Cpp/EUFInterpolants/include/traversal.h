a29325e448476bb793c0ece437a1c26a96fa1c6c
typesAreSpaces <jabelcastellanosjoo@unm.edu>
2018-07-19 21:51:05 +0800

    Changes

---
+++ Software/Cpp/EUFInterpolants/include/traversal.h
@@ -0,0 +1,10 @@
+#ifndef _TRAVERSAL_
+#define _TRAVERSAL_
+
+#include "z3++.h"
+#include <stack>
+
+void visit(z3::expr const &);
+void visitWithStack(z3::expr &);
+
+#endif
--- Software/Cpp/EUFInterpolants/src/main.cpp
+++ Software/Cpp/EUFInterpolants/src/main.cpp
@@ -1,8 +1,36 @@
-#include <iostream>
+/*++
+  Copyright (c) 2015 Microsoft Corporation
 
-int main(int argc, char ** argv){
+  --*/
 
+#include "z3++.h"
+#include "traversal.h"
+
+void tst_visit() {
+    std::cout << "visit example\n";
+    z3::context c;
+
+    z3::expr x = c.int_const("x");
+    z3::expr y = c.int_const("y");
+    z3::expr z = c.int_const("z");
+    z3::expr f = x*x - y*y >= 0;
+
+    visit(f);
+}
+
+int main(int argc, char ** argv) {
+
+  std::string file = "./SMT2_files/interpolantExample1.smt2";
+  z3::config cfg;
   
+  cfg.set("PROOF", true);
+  cfg.set("MODEL", true);
+  cfg.set("TRACE", false);
+  z3::context ctx(cfg);
+  
+  z3::expr inputFormula = ctx.parse_file(file.c_str());
+  visitWithStack(inputFormula);
+  //visit(inputFormula);
   
   return 0;
 }
