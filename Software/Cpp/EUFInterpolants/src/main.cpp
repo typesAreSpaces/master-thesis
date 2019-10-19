c9ab06d576944d37746d7cabeca682162bc0da73
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2019-10-19 15:41:02 -0600

    Add extra code to find the unique sort of the formulas used in EUF.

--- Software/Cpp/EUFInterpolants/src/main.cpp
+++ Software/Cpp/EUFInterpolants/src/main.cpp
@@ -17,27 +17,34 @@ int main(int argc, char ** argv){
   
   if(argc >= 2) {
     z3::context ctx;
+    z3::expr input_formula = ctx.parse_file(argv[1])[0];
+    z3::expr aux_expr = input_formula;
     std::set<std::string> symbols_to_elim;
     for(int index = 2; index < argc; ++index)
       symbols_to_elim.insert(argv[index]);
-    z3::expr input_formula = ctx.parse_file(argv[1])[0];
 
-    // std::cout << input_formula.arg(0).arg(1).decl().range().id() << std::endl;
-    
-    CongruenceClosure cc_example(ctx, input_formula, symbols_to_elim, 0);
-    std::cout << cc_example << std::endl;
-    cc_example.addEquation(6, 11);
-    std::cout << cc_example << std::endl;
+    // Find a leave node in input_formula
+    // in order to find the sort
+    // of every expression (convention)
+    while(aux_expr.num_args() != 0)
+      aux_expr = aux_expr.arg(0);
 
-    CongruenceClosure cc_example2(ctx, input_formula, symbols_to_elim, 1);
-    cc_example.transferEqClassAndPreds(cc_example2);
-    std::cout << cc_example << std::endl;
+    // std::cout <<  << std::endl;
     
-    // EUFInterpolant euf_interpolant_example(input_formula,
-    // 					   symbols_to_elim,
-    // 					   ctx.uninterpreted_sort("A"));
-    // std::cout << "The Interpolant" << std::endl;
-    // euf_interpolant_example.test(); 
+    // CongruenceClosure cc_example(ctx, input_formula, symbols_to_elim, 0);
+    // std::cout << cc_example << std::endl;
+    // cc_example.addEquation(6, 11);
+    // std::cout << cc_example << std::endl;
+
+    // CongruenceClosure cc_example2(ctx, input_formula, symbols_to_elim, 1);
+    // cc_example.transferEqClassAndPreds(cc_example2);
+    // std::cout << cc_example << std::endl;
+    
+    EUFInterpolant euf_interpolant_example(input_formula,
+    					   symbols_to_elim,
+					   aux_expr.decl().range());
+    std::cout << "The Interpolant" << std::endl;
+    euf_interpolant_example.test(); 
   }
   return 0;
 }
