375a575c76479d9555bf7c0c7e0aecc014928636
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2020-02-29 23:29:12 -0700

    Test UnionFindExplain implementation.

--- Software/Cpp/EUFInterpolantsZ3/src/main.cpp
+++ Software/Cpp/EUFInterpolantsZ3/src/main.cpp
@@ -13,57 +13,29 @@ void test2();
 
 int main(int argc, char ** argv){
  
-  // z3::context ctx;
-  // z3::sort my_sort = ctx.uninterpreted_sort("A");
-  // z3::expr z1 = ctx.constant("z1", my_sort);
-  // z3::expr z2 = ctx.constant("z2", my_sort);
-  // z3::expr y1 = ctx.constant("y1", my_sort);
-  // z3::expr y2 = ctx.constant("y2", my_sort);
-  // z3::expr s1 = ctx.constant("s1", my_sort);
-  // z3::expr s2 = ctx.constant("s2", my_sort);
-  // z3::expr v = ctx.constant("v", my_sort);
-  // z3::expr t = ctx.constant("t", my_sort);
-  // z3::func_decl f = ctx.function("f", my_sort, my_sort, my_sort);
-  // z3::func_decl g = ctx.function("g", my_sort, my_sort);
-  // std::set<std::string> uncomms;
-  // uncomms.insert("v");
+  z3::context ctx;
+  z3::sort my_sort = ctx.uninterpreted_sort("A");
+  z3::expr z1 = ctx.constant("z1", my_sort);
+  z3::expr z2 = ctx.constant("z2", my_sort);
+  z3::expr y1 = ctx.constant("y1", my_sort);
+  z3::expr y2 = ctx.constant("y2", my_sort);
+  z3::expr s1 = ctx.constant("s1", my_sort);
+  z3::expr s2 = ctx.constant("s2", my_sort);
+  z3::expr v = ctx.constant("v", my_sort);
+  z3::expr t = ctx.constant("t", my_sort);
+  z3::func_decl f = ctx.function("f", my_sort, my_sort, my_sort);
+  z3::func_decl g = ctx.function("g", my_sort, my_sort);
+  std::set<std::string> uncomms;
+  uncomms.insert("v");
   
-  // // z3::expr alpha = f(z1, v) == s1 && f(z2, v) == s2 && f(f(y1, v), f(y2, v)) == t && s1 != t;
-  // z3::expr alpha = f(z1, v) == s1 && f(z2, v) == s2 && f(f(y1, v), f(y2, v)) == t && s1 != t && g(g(s1)) == s2 && g(g(f(y1, v))) == f(y2, v);
-  // z3::expr r_alpha = rename(alpha, uncomms);
+  // z3::expr alpha = f(z1, v) == s1 && f(z2, v) == s2 && f(f(y1, v), f(y2, v)) == t && s1 != t;
+  z3::expr alpha = f(z1, v) == s1 && f(z2, v) == s2 && f(f(y1, v), f(y2, v)) == t && s1 != t && g(g(s1)) == s2 && g(g(f(y1, v))) == f(y2, v);
+  z3::expr r_alpha = rename(alpha, uncomms);
   
-  // EUFInterpolant euf(r_alpha);
-  // // std::cout << euf << std::endl;
+  EUFInterpolant euf(r_alpha);
+  // std::cout << euf << std::endl;
     
-  // // euf.buildInterpolant();
-
-  UnionFindExplain a(10);
-  a.merge(1, 0);
-  a.merge(0, 2);
-  a.merge(4, 3);
-  a.merge(4, 5);
-  a.merge(2, 6);
-  a.merge(5, 2);
-
-  std::cout << "Explaining 6, 5" << std::endl;
-  for(auto x : a.explain(6, 5))
-    std::cout << x << std::endl;
-
-  std::cout << "Explaining 5, 6" << std::endl;
-  for(auto x : a.explain(5, 6))
-    std::cout << x << std::endl;
-
-  std::cout << "Explaining 1, 6" << std::endl;
-  for(auto x : a.explain(1, 6))
-    std::cout << x << std::endl;
-
-  std::cout << "Explaining 6, 1" << std::endl;
-  for(auto x : a.explain(6, 1))
-    std::cout << x << std::endl;
-
-  std::cout << "Explaining 6, 9" << std::endl;
-  for(auto x : a.explain(6, 9))
-    std::cout << x << std::endl;
+  // euf.buildInterpolant();
   
   return 0;
 }
