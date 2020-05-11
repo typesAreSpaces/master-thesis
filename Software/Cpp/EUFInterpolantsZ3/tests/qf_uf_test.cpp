4b9d2e7a862ede72b30bf8fb046863b20c6dc9c4
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2020-05-10 23:03:14 -0600

    Minor changes.

--- Software/Cpp/EUFInterpolantsZ3/tests/qf_uf_test.cpp
+++ Software/Cpp/EUFInterpolantsZ3/tests/qf_uf_test.cpp
@@ -5,11 +5,11 @@ int main(int argc, char ** argv){
 
   if(argc == 2){
     z3::context ctx;
-    auto input = mk_and(ctx.parse_file(argv[1]));
+    auto assertions = ctx.parse_file(argv[1]);
     std::string input_file = argv[1];
-    TestCongruenceClosureExplain test(input);
+    TestCongruenceClosureExplain test(assertions);
     try {
-      test.testConsistency(input, 1);
+      test.testConsistency(assertions, 1);
     }
     catch(char const * e){
       std::cout << e << std::endl;
