6b237dae62f2f08bb1b1b68027f8af20dc0976f9
jose <jabelcastellanosjoo@unm.edu>
2020-05-28 11:47:04 -0600

    Fix GeneralizedCartesianProduct(std::vector<std::vector<T>> const &) in Utill.h

--- Software/Cpp/EUFInterpolantsZ3/include/Util.h
+++ Software/Cpp/EUFInterpolantsZ3/include/Util.h
@@ -8,22 +8,19 @@ bool compareEquation(const z3::expr &, const z3::expr &);
 bool compareTerm(const z3::expr &, const z3::expr &);
 
 template<typename T>
-std::vector<std::vector<T>> GeneralizedCartesianProduct(std::vector<std::vector<T>> const & x){
+std::vector<std::vector<T> > GeneralizedCartesianProduct(std::vector<std::vector<T> > const & x){
   unsigned ans_size = 1;
   for(auto const & entry : x)
     ans_size *= entry.size();
 
-  std::vector<std::vector<T>> ans(ans_size, std::vector<T>(0));
-  if(!ans_size)
+  std::vector<std::vector<T> > ans(ans_size, std::vector<T>(0));
+  if(ans_size)
     for(auto const & entry : x){
       auto block_size = ans_size/entry.size();
-      unsigned i = 1;
-      unsigned j = 0;
+      unsigned i = 1, j = 0;
       for(auto const & value : entry){
-        while(j < i*block_size){
-          ans[j].push_back(value);
-          ++j;
-        }
+        while(j < i*block_size)
+          ans[j++].push_back(value);
         ++i;
       }
     }
@@ -32,7 +29,7 @@ std::vector<std::vector<T>> GeneralizedCartesianProduct(std::vector<std::vector<
 }
 
 template<typename T>
-void printGeneralizedCartesianProduct(std::vector<std::vector<T>> const & x){
+void printGeneralizedCartesianProduct(std::vector<std::vector<T> > const & x){
   for(auto const & entry : x){
     for(auto const & value : entry)
       std::cout << value << " ";
