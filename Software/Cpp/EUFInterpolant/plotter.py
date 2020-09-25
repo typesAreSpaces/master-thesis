2b867600689ab0fd08157dca87a6f544c6745e17
jose <jabelcastellanosjoo@unm.edu>
2020-09-25 11:40:32 -0600

    Add plot of EUFI performance comparison.

--- Software/Cpp/EUFInterpolant/plotter.py
+++ Software/Cpp/EUFInterpolant/plotter.py
@@ -26,19 +26,38 @@ def parse_info_chunks(file_names, _data, num_test):
     return _data
 
 
-# num_tests = 10000
-num_tests = 6000
+num_tests = 10000
+# num_tests = 6000
 
 steps = np.arange(0, num_tests)
-data_iz3 = parse_info("iz3_results.txt", 
+data_iz3 = parse_info("results/iz3_results.txt", 
         np.zeros(num_tests, dtype=float), num_tests)
-data_mathsat = parse_info("mathsat_results.txt", 
+data_mathsat = parse_info("results/mathsat_results.txt", 
         np.zeros(num_tests, dtype=float), num_tests)
 data_eufi = parse_info_chunks([
-    "eufi_results_0_upto_6000.txt", 
-    "eufi_results_1_upto_6000.txt", 
-    "eufi_results_2_upto_6000.txt",  
-    "eufi_results_3_upto_6000.txt"],np.zeros(num_tests, dtype=float), num_tests)
+    "results/eufi_results_0_upto_6000.txt", 
+    "results/eufi_results_1_upto_6000.txt", 
+    "results/eufi_results_2_upto_6000.txt",  
+    "results/eufi_results_3_upto_6000.txt", 
+    "results/eufi_results_0.txt",
+    "results/eufi_results_1.txt",
+    "results/eufi_results_2.txt",
+    "results/eufi_results_3.txt",
+    "results/eufi_results_4.txt",
+    "results/eufi_results_5.txt",
+    "results/eufi_results_6.txt",
+    "results/eufi_results_7.txt",
+    "results/eufi_results_8.txt",
+    "results/eufi_results_9.txt",
+    "results/eufi_results_10.txt",
+    "results/eufi_results_11.txt",
+    "results/eufi_results_12.txt",
+    "results/eufi_results_13.txt",
+    "results/eufi_results_14.txt",
+    "results/eufi_results_15.txt",
+    "results/eufi_results_16.txt",
+    "results/eufi_results_17.txt",
+    ],np.zeros(num_tests, dtype=float), num_tests)
 
 fig, ax = plt.subplots()
 ax.scatter(steps, data_eufi, marker='x', color='red', label='EUF Uniform Interpolator')
@@ -51,4 +70,4 @@ ax.set_title('Performance comparison of interpolant generation algorithms for EU
 
 legend = ax.legend(loc='upper left', shadow=True, fontsize='x-large')
 
-plt.show()
+plt.savefig("results/eufi_performance_graph.png")
